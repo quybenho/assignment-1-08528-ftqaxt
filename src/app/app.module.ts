41d7143728d6d2dbdef9ecf42fec1f3382383cb1
vanlaanh <quyndpph08528@fpt.edu.vn>
2020-04-16 19:35:15 +0700

    1 file modified, 2 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { HomeComponent } from './home/home.component';
-import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductNewsComponent } from './product-news/product-news.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
@@ -16,9 +17,10 @@ import { ProductEditComponent } from './product-edit/product-edit.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductContactComponent } from './product-contact/product-contact.component';
 import { ProductSliderComponent } from './product-slider/product-slider.component';
+import { AdminComponent } from './admin/admin.component';
 @NgModule({
   imports: [BrowserModule, FormsModule, HttpClientModule, AppRoutingModule, NgbModule],
-  declarations: [AppComponent, HelloComponent, HomeComponent, ProductListComponent, ProductNewsComponent, ProductDetailComponent, ProductManagerComponent, ProductAddComponent, ProductEditComponent, NotFoundComponent, ProductContactComponent, ProductSliderComponent],
+  declarations: [AppComponent, HelloComponent, HomeComponent, ProductListComponent, ProductNewsComponent, ProductDetailComponent, ProductManagerComponent, ProductAddComponent, ProductEditComponent, NotFoundComponent, ProductContactComponent, ProductSliderComponent, AdminComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
